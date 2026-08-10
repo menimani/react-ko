e8996a1e5d9255172669f29e5d639518fe954e75
menimani <github@menimani.jp>
2026-08-10 17:08:45 +0900

    fix: allow nullish app view models

--- src/react-ko/src/context/AppViewModelContext.ts
+++ src/react-ko/src/context/AppViewModelContext.ts
@@ -1,10 +1,12 @@
 import { createContext, useContext } from 'react'
 
+const MISSING_APP_VIEW_MODEL = Symbol('MISSING_APP_VIEW_MODEL')
+
 /**
  * Context to store the global AppViewModel.
  * Intended to be overridden by consumer's ViewModel type.
  */
-export const AppViewModelContext = createContext<unknown | null>(null)
+export const AppViewModelContext = createContext<unknown>(MISSING_APP_VIEW_MODEL)
 
 /**
  * Hook to access the current AppViewModel.
@@ -12,7 +14,7 @@ export const AppViewModelContext = createContext<unknown | null>(null)
  */
 export function useAppViewModel<T>(): T {
   const context = useContext(AppViewModelContext)
-  if (context === null) {
+  if (context === MISSING_APP_VIEW_MODEL) {
     throw new Error('useAppViewModel must be used within an AppViewModelContext.Provider.')
   }
   return context as T
